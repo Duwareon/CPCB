08307cdc214c1dedb3dbcb445349b62af6d4874b
Duwareon <duwareon@gmail.com>
2025-02-05 17:49:13 -0500

    Added shading to fractals

--- Fractals/main.jl
+++ Fractals/main.jl
@@ -1,15 +1,15 @@
-#!/usr/bin/env julia
-using Luxor, Colors, Images
+#!/usr/bin/env julia 
+using Images
 
 function explodes(f, z, mu, tau, iterations)
 	for i in 1:iterations
 		z = f(z, mu)
 		if abs(z) > abs(tau)
-			return true
+			return true, i
 			continue
 		end
 	end
-	return false
+	return false, 0
 end
 
 function get_range(center, scale, res)
@@ -20,14 +20,15 @@ function fractal(f, mu, tau, iterations, xres, yres, xcenter, ycenter, xscale, y
 	data = Vector{Vector{ComplexF64}}()
 	for i in 1:Threads.nthreads() push!(data, ComplexF64[]) end
 
-	image = ones(ARGB32, xres, yres)
+	image = zeros(Float64, yres, xres)
 
 	Threads.@threads for (i, x) in collect(enumerate(get_range(xcenter, xscale, xres)))
 		for (j, y) in collect(enumerate(get_range(ycenter, yscale, yres)))
 			z = x + (y)im
-			if !explodes(f, z, mu, tau, iterations)
+			cond, final = explodes(f, z, mu, tau, iterations)
+			if cond
 				push!(data[Threads.threadid()], z)
-				image[i, j] = colorant"black"
+				image[j, i] = (final/iterations)
 			end
 		end
 	end
@@ -35,20 +36,24 @@ function fractal(f, mu, tau, iterations, xres, yres, xcenter, ycenter, xscale, y
 	return data_combined, image
 end
 
-f(z, mu) = sin(z) + 1/z + mu
-mu = 0.1 - 0.2im
+f(z, mu) = (z^5) + (z^3) + mu
+mu = (-0.7-0.5im)*0.8
 
 res = 10000
+scale = [1.0, 1.5]
 center = [0, 0]
-scale = [1,1.2]
+
+xres = trunc(Int, res*scale[1])
+yres = trunc(Int, res*scale[2])
 
 tau = 100
-iterations = 100
+iterations = 30
 
 runtime = @elapsed begin
-	data, image = fractal(f, mu, tau, iterations, res, res, center[1], center[2], scale[1], scale[2])
+	data, image = fractal(f, mu, tau, iterations, xres, yres, center[1], center[2], scale[1], scale[2])
 end
-println("runtime: ", runtime, ", points: ", length(data))
+println("runtime: $runtime")
 
-Drawing(image, "output.png")
-finish()
+save("output.png", image)
+#plot(image)
+#readline()
